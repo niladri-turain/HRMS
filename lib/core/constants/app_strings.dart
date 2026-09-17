b3849fc8a98a0a96cb3380dfc0f0f0e1ba0b0c0b
niladri-turain <niladri@turaingrp.com>
2026-09-17 13:19:24 +0530

    commet line token

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -1,5 +1,5 @@
 class AppStrings {
   static const appName = "HRMS";
-  //static const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoibmlsYWRyaTE5OTYiLCJhIjoiY211NTV1bWxlMG45ejJ3cXZyYmFtN2lnOSJ9.XH5-RUlsbr3ztLUwZznXIw";
+
   static const MAPBOX_ACCESS_TOKEN = "ADD MAPBOX TOKEN";
 }
